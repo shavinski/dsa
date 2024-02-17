560cd6da11544a2769c0999a9d7f414bf09b6ded
Jakob <shavinski.jakob@gmail.com>
2024-02-16 16:42:23 -0800

    Input the most optimal solution

--- arraysAndHashing/gropuAnagrams.py
+++ arraysAndHashing/gropuAnagrams.py
@@ -50,3 +50,14 @@ class Solution(object):
 
         return letter_count
 
+# HERE IS THE MOST OPTIMAL SOLUTION
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        ans = collections.defaultdict(list)
+
+        for s in strs:
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord("a")] += 1
+            ans[tuple(count)].append(s)
+        return ans.values()
